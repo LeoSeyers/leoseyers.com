2d1c5fc72277a9cd34771073d8ed2e3a247d6d96
Leo Seyers <leoseyers@gmail.com>
2019-05-21 17:11:48 +0200

    page title defined in page component instead of nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,6 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
