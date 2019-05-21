bfc980925c516e5fd0b2ae3283d8cee307f0031b
Leo Seyers <leoseyers@gmail.com>
2019-05-21 21:16:00 +0200

    issue : color name converted to hex

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
-    '@nuxtjs/style-resources'
+    // '@nuxtjs/style-resources'
   ],
 
   // styleResources: {
