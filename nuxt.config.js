92727224b369cdeff09c411fd96707362205a8fe
Leo Seyer] <leo@studio-scale.com>
2019-09-16 14:56:14 +0200

    clean config and dependecies

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,18 @@
 import { headConfig }  from "./plugins/head";
 
 export default {
+
   mode: "universal",
-
   head: headConfig,
-
   layoutTransition: "intro",
-
   loading: { color: "#e3e3e3", height: "5px" },
-
   css: ["@/assets/scss/main.scss"],
 
   modules: [
     "@nuxtjs/axios",
     "@nuxtjs/pwa",
     "@nuxtjs/sitemap",
-    "nuxt-webfontloader"
+    "nuxt-webfontloader",
   ],
 
   webfontloader: {
